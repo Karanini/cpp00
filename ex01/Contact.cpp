55311810bfafe31281dc926ba6eb4f29af656522
bkaras-g <bkaras-g@student.42.fr>
2026-01-20 17:58:11 +0100

    add_user() wip experimenting with std::getline()

--- ex01/Contact.cpp
+++ ex01/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: bkaras-g <bkaras-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 17:06:18 by bkaras-g          #+#    #+#             */
-/*   Updated: 2026/01/20 17:22:19 by bkaras-g         ###   ########.fr       */
+/*   Updated: 2026/01/20 17:56:20 by bkaras-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,27 @@
 
 void	Contact::add_user()
 {
+	// std::cout << "Enter first name:\n";
+	// std::cin >> this->first_name;
+	// std::cout << "Enter last name:\n";
+	// std::cin >> this->last_name;
+	// std::cout << "Enter nickname:\n";
+	// std::cin >> this->nickname;
+	// std::cout << "Enter phone number:\n";
+	// std::cin >> this->phone_number;
+	// std::cout << "What is your contact's DARKEST secret ?\n";
+	// std::cin >> this->darkest_secret;
+
 	std::cout << "Enter first name:\n";
-	std::cin >> this->first_name;
-	std::cout << "Enter last name:\n";
-	std::cin >> this->last_name;
-	std::cout << "Enter nickname:\n";
-	std::cin >> this->nickname;
-	std::cout << "Enter phone number:\n";
-	std::cin >> this->phone_number;
-	std::cout << "What is your contact's DARKEST secret ?\n";
-	std::cin >> this->darkest_secret;
+    std::getline(std::cin, this->first_name);
+    std::cout << "Enter last name:\n";
+    std::getline(std::cin, this->last_name);
+    std::cout << "Enter nickname:\n";
+    std::getline(std::cin, this->nickname);
+    std::cout << "Enter phone number:\n";
+    std::getline(std::cin, this->phone_number);
+    std::cout << "What is your contact's DARKEST secret ?\n";
+	std::getline(std::cin, this->darkest_secret);
 
 	std::cout << this->first_name;
 	std::cout << '\n';
--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: bkaras-g <bkaras-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 16:54:36 by bkaras-g          #+#    #+#             */
-/*   Updated: 2026/01/20 17:08:39 by bkaras-g         ###   ########.fr       */
+/*   Updated: 2026/01/20 17:56:17 by bkaras-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ private:
 	std::string first_name;
 	std::string last_name;
 	std::string	nickname;
-	int			phone_number;
+	std::string	phone_number;
 	std::string	darkest_secret;
 public:
 	void add_user();
