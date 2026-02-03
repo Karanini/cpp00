019d9405221ecafb5b9938e0341c0ea8e2ff493f
bkaras-g <bkaras-g@student.42.fr>
2026-02-03 14:00:57 +0100

    handle empty line input
    use getline() to retrieve user input to be able to catch an empty line
    use empty() to detect empty line

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bkaras-g <bkaras-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 17:12:58 by bkaras-g          #+#    #+#             */
-/*   Updated: 2026/02/03 13:25:51 by bkaras-g         ###   ########.fr       */
+/*   Updated: 2026/02/03 14:00:37 by bkaras-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,17 @@ int	main()
 	while (true)
 	{
 		std::cout << "Hi! Welcome to the super Phone Book app! What do you want do to today ?\n";
-		std::cin >> user_input;
+		std::getline(std::cin, user_input); //instead of std::cin >> user_input; to be able to catch an empty line
 		if (std::cin.eof())
-        {
-            std::cout << "\nExiting due to Ctrl+D..." << std::endl;
-            break;
-        }
-		std::cin.ignore(INT_MAX, '\n');
+		{
+			std::cout << "\nExiting due to Ctrl+D..." << std::endl;
+			break;
+		}
+		if (user_input.empty())
+		{
+			std::cout << "Empty input — please type 'ADD', 'SEARCH' or 'EXIT'.\n";
+			continue;
+		}
 		if (!user_input.compare("ADD"))
 		{
 			phonebook.add_user();
